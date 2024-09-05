6a45fa94fbd393948ad185439a0ca4e318a654b7
Guillermo Pineda <13656292+memo-pineda@users.noreply.github.com>
2024-09-05 15:59:10 -0700

    MVP

---
+++ src/index.tsx
@@ -0,0 +1,115 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+
+interface TabItem {
+  id: string;
+  displayName: string;
+  onClick: () => void;
+}
+
+interface Tabs {
+  tabItems: Array<TabItem>;
+}
+
+interface EditTab {
+  appConnection: string;
+}
+
+const Tabs: React.FC<Tabs> = ({ tabItems }) => {
+  return <div className="flex flex-col gap-1">
+    {tabItems.map((tabItem) => <button id={tabItem.id} onClick={tabItem.onClick}>{tabItem.displayName}</button>)}
+  </div>
+}
+
+const Tab = {
+  HOME: 'HOME',
+  EDIT: 'EDIT'
+}
+
+const EditTab: React.FC<EditTab> = ({ appConnection }) => {
+  const [resource, setResource] = useState('');
+
+  useEffect(() => {
+    async function fetchAppResource() {
+      if (appConnection === '') {
+        return;
+      }
+
+      const res = await webflow.getCurrentAppConnectionResource();
+      if (res && res.type === 'Element') {
+        console.log("res", res);
+        setResource(JSON.stringify(res.value.id));
+      }
+    }
+
+    void fetchAppResource();
+  }, [appConnection])
+
+  if (appConnection === '') {
+    return <p>Missing app connection, try launching app from Element Panel</p>
+  }
+
+  return <>
+    <p>App Connection: {appConnection}</p>
+    <p>Id: {resource}</p>
+  </>
+}
+
+const App: React.FC = () => {
+  const [currentTab, setCurrentTab] = useState(Tab.HOME);
+  const [currentAppConnection, setCurrentAppConnection] = useState('');
+
+  useEffect(() => {
+    async function retrieveAppConnections() {
+      const appConnections = await webflow.getCurrentAppConnection();
+      console.log('app connections', appConnections);
+      if (['manageImageElement', 'manageFormElement'].includes(appConnections)) {
+        setCurrentTab(Tab.EDIT);
+        setCurrentAppConnection(appConnections);
+      }
+    }
+
+    void retrieveAppConnections();
+  }, [])
+
+  const goToTab = (tab) => {
+    setCurrentTab(tab);
+  }
+
+  const addAppConnection = async (type: ElementPreset<AnyElement>) => {
+    const root = await webflow.getRootElement();
+    if (root && root.children) {
+      const el = await root.append(type);
+      if (!el || !el.appConnections) {
+        throw new Error("App Connections not supported");
+      }
+      await el.setAppConnection(type == webflow.elementPresets.Image ? 'manageImageElement' : 'manageFormElement');
+    } else {
+      throw new Error("Expected an element.")
+    }
+  }
+
+  return (
+    <div>
+      <h1>App Connections Tester</h1>
+      <Tabs tabItems={[
+        {id: Tab.HOME, displayName: 'Create Elements', onClick: () => goToTab(Tab.HOME)},
+        {id: Tab.EDIT, displayName: 'Edit Elements', onClick: () => goToTab(Tab.EDIT)}
+      ]}/>
+      {currentTab === Tab.HOME && (
+        <>
+        <p>Click button to create element of specified type and apply an app connection</p>
+        <button onClick={() => addAppConnection(webflow.elementPresets.FormForm)}>Form</button>
+        <button onClick={() => addAppConnection(webflow.elementPresets.Image)}>Image</button>
+        </>
+      )}
+      {currentTab === Tab.EDIT && (
+        <>
+          <EditTab appConnection={currentAppConnection}/>
+        </>
+      )}
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
